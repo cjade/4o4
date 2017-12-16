00287ccad56080ba88d1d4baab6d285f6e837964
Jade <862226994@qq.com>
2017-12-16 10:05:15 +0800

    admin

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/assets/home/js/app.js', 'public/js')
+    .js('resources/assets/admin/js/admin.js', 'public/js')
+   .sass('resources/assets/home/sass/app.scss', 'public/css')
+   .sass('resources/assets/admin/sass/admin.scss', 'public/css');
